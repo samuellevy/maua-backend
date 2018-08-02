32141a7c6f4968e92c6acf897794316167e86453
Samuel Levy <samuellevy@3AWW13.local>
2018-08-02 13:12:16 -0300

    upload csv into sales table

--- src/Controller/Admin/SalesController.php
+++ src/Controller/Admin/SalesController.php
@@ -6,8 +6,6 @@ use App\Controller\AppControllerAdmin;
 class SalesController extends AppController
 {
   public function load(){
-    $sales = $this->Sales->newEntity();
-
     if ($this->request->is('post')) {
       $stores = Array();
       $data = $this->request->getData();
@@ -19,12 +17,51 @@ class SalesController extends AppController
       }
       fclose($file);
 
-      die(debug($stores));
+      $sales = [];
+      foreach($stores as $key=>$store){
+        if($key==0){
+
+        }else{
+          array_push($sales, ['store_id'=>$store[0],'month'=>8, 'goal' => $store[4], 'quantity' => $store[5]]);
+          array_push($sales, ['store_id'=>$store[0],'month'=>9, 'goal' => $store[6], 'quantity' => $store[7]]);
+          array_push($sales, ['store_id'=>$store[0],'month'=>10, 'goal' => $store[8], 'quantity' => $store[9]]);
+          array_push($sales, ['store_id'=>$store[0],'month'=>11, 'goal' => $store[10], 'quantity' => $store[11]]);
+          array_push($sales, ['store_id'=>$store[0],'month'=>12, 'goal' => $store[12], 'quantity' => $store[13]]);
+        }
+      }
+
+      $this->Sales->truncate();
+      foreach($sales as $sale){
+        $salesEntity = $this->Sales->newEntity();
+        $salesEntity = $this->Sales->patchEntity($salesEntity, $sale);
+        $this->Sales->save($salesEntity);
+      }
+      
+      // foreach($stores as $key=>$store){
+      //   if($key==0){
+
+      //   }else{
+      //     $stores[$key]['sales']=[];
+      //     array_push($stores[$key]['sales'], ['month'=>8, 'goal' => $store[4], 'quantity' => $store[5]]);
+      //     array_push($stores[$key]['sales'], ['month'=>9, 'goal' => $store[6], 'quantity' => $store[7]]);
+      //     array_push($stores[$key]['sales'], ['month'=>10, 'goal' => $store[8], 'quantity' => $store[9]]);
+      //     array_push($stores[$key]['sales'], ['month'=>11, 'goal' => $store[10], 'quantity' => $store[11]]);
+      //     array_push($stores[$key]['sales'], ['month'=>12, 'goal' => $store[12], 'quantity' => $store[13]]);
+      //     // $stores[$key]['sales']=['quantity'=>$store[4]];
+      //   }
+      // }
+
+      // $sales->store_id = 2;
+      // $this->Sales->truncate();
+      // $this->Sales->save($sales);
+      die(debug($sales));
+      // die(debug($this->Sales->truncate()));
     }
 
-    $array1 = array("bola", "quadrado", "triangulo");
-    $array2 = array("esfera", "quadrado", "triangulo");
+    
 
+    $array1 = array(0=>"bola", 1=>"quadrado", 2=>"triangulo");
+    $array2 = array(0=>"esfera", 1=>"quadrado", 2=>"triangulo");
     $result = array_diff($array1, $array2);
     print_r($result);
 
--- src/Model/Table/SalesTable.php
+++ src/Model/Table/SalesTable.php
@@ -5,6 +5,7 @@ use Cake\ORM\Query;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
+use Cake\Datasource\ConnectionManager;
 
 /**
  * Sales Model
@@ -69,17 +70,19 @@ class SalesTable extends Table
         return $validator;
     }
 
-    /**
-     * Returns a rules checker object that will be used for validating
-     * application integrity.
-     *
-     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
-     * @return \Cake\ORM\RulesChecker
-     */
     public function buildRules(RulesChecker $rules)
     {
         $rules->add($rules->existsIn(['store_id'], 'Stores'));
 
         return $rules;
+    }
+
+    public function truncate(){
+        $conn = ConnectionManager::get('default');
+        if($conn->execute('TRUNCATE TABLE sales')){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
